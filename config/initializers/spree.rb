5825ebeb203fbaeccdb67c717d5de2c0eedca052
pstrzalk <pawel@itideas.pl>
2017-05-11 22:36:13 +0200

    Remove create! from initializer/spree

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -5,7 +5,7 @@ Spree::PermittedAttributes.shipment_attributes.push :giftmessage, :giftwrap_id
 Rails.application.config.spree.adjusters << Spree::Adjustable::Adjuster::Giftwrap
 
 # Init first Giftwrap if not available
-Spree::Giftwrap.create!(price: 5, title: "Add Gift Wrapping") unless Spree::Giftwrap.any?
+# Spree::Giftwrap.create!(price: 5, title: "Add Gift Wrapping") unless Spree::Giftwrap.any?
 
 # Additional attributes for API
 Spree::Api::ApiHelpers.shipment_attributes.push :giftwrap, :giftwrap_total, :giftmessage
