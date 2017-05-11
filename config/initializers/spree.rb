d85837cbae2f5e3edf2283a93909ff1dc0eeabe1
pstrzalk <pawel@itideas.pl>
2017-05-11 16:18:36 +0200

    API Attributes

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,3 +6,7 @@ Rails.application.config.spree.adjusters << Spree::Adjustable::Adjuster::Giftwra
 
 # Init first Giftwrap if not available
 Spree::Giftwrap.create!(price: 5, title: "Add Gift Wrapping") unless Spree::Giftwrap.any?
+
+# Additional attributes for API
+Spree::Api::ApiHelpers.shipment_attributes.push :giftwrap, :giftwrap_total, :giftmessage
+Spree::Api::ApiHelpers.order_attributes.push :giftwrap_total
