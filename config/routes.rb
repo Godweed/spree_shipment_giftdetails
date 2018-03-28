98c0955e709a63c80acdc90f7259522e4261dd39
Wojtek <swierczynskiwojtek@gmail.com>
2018-03-28 10:56:28 +0200

    shipment gift details form

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Spree::Core::Engine.routes.draw do
+  namespace :admin do
+    patch 'update_shipment_gift_details' => 'orders#update_shipment_gift_details', as: 'update_shipment_gift_details'
+  end
 end
